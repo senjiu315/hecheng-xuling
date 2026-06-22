f43f118267b8ed0ed435dc2d225cc3a8a80a3a61
senjiu315 <Ly19996000@163.com>
2026-06-22 22:10:25 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "hecheng-xuling-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./main.js",
+  "./manifest.webmanifest",
+  "./assets/photos/cover.jpg",
+  "./assets/photos/level-1.jpg",
+  "./assets/photos/level-2.jpg",
+  "./assets/photos/level-3.jpg",
+  "./assets/photos/level-4.jpg",
+  "./assets/photos/level-5.jpg",
+  "./assets/photos/level-6.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
